4cdeb94d772d1a51b030089897a0afa4251889b8
FilipM <fm@air.local>
2020-05-11 14:10:57 +0200

    Updates

---
+++ precache-manifest.90f4841686257961c39dee6e59493ec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe446f3aadd17c4f2c70ecf0f7735ffa",
+    "url": "/corona_tracker/index.html"
+  },
+  {
+    "revision": "a3ef30d8ced65ef04f27",
+    "url": "/corona_tracker/static/css/main.1c5735d4.chunk.css"
+  },
+  {
+    "revision": "5271590177569e48d797",
+    "url": "/corona_tracker/static/js/2.2e4e880b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker/static/js/2.2e4e880b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ef30d8ced65ef04f27",
+    "url": "/corona_tracker/static/js/main.0b3c45a7.chunk.js"
+  },
+  {
+    "revision": "4f6b5d8abe178acc51e1",
+    "url": "/corona_tracker/static/js/runtime-main.65b451b5.js"
+  }
+]);
